2663cf1cd18b394434c0d4d7f6e0efb644e8c562
LainWeire <lucasfrota3000@gmail.com>
2024-11-18 08:51:08 -0300

    uff provas

---
+++ login.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+from time import sleep
+
+st.text("Como deseja se registrar?")
+
+def ver_preenchidos(campos): 
+    return all(campos.values())
+
+foto = st.file_uploader("Foto do aluno:", type=["jpg", "jpeg", "png"])
+nome = st.text_input("Nome do aluno:", value="", placeholder="Digite aqui...")
+cpf = st.text_input("CPF do aluno:", value="", placeholder="Digite aqui...")
+responsavel = st.text_input("Responsável do aluno:", value="", placeholder="Digite aqui...")
+id_es = st.text_input("Número ID da escola:", value="", placeholder="Digite aqui...")
+
+if foto is not None:
+    st.image(foto, caption="Foto do aluno", width=100)
+
+campos_aluno = {
+    'Nome': nome,
+    'CPF': cpf,
+    'Responsável': responsavel,
+    'Foto': foto
+}
+
+if st.button("Concluir"):
+    if ver_preenchidos(campos_aluno):
+        st.success("Concluído com sucesso!")
+        sleep(0.5)
+        st.switch_page("pages/home_aluno.py")
+    
+    else:
+        st.error("Por favor, preencha todos os campos.")
+    
+
---
+++ pages/home_aluno.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+from time import sleep
+
+
+st.title("Bem vindo!")
+
+st.write(
+    """
+
+    uff referencias 🔥🫎🫎
+
+""")
