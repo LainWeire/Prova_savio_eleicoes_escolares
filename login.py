bd877f609abad4f2af843ef6d7c5d2e5e981f66f
𝕷𝖚𝖈𝖆𝖘 <lucasfrota3000@gmail.com>
2024-11-19 13:13:23 -0300

    Update login.py

--- login.py
+++ login.py
@@ -8,6 +8,8 @@ def ver_preenchidos(campos):  # funcao que ve se cada entrada foi preenchida
     return all(campos.values())
 
 
+
+
 foto = st.file_uploader("Foto do aluno:", type=["jpg", "jpeg", "png"])
 nome = st.text_input("Nome do aluno:", value="", placeholder="Digite aqui...")
 cpf = st.text_input("CPF do aluno:", value="", placeholder="Digite aqui...")
@@ -17,19 +19,36 @@ id_es = st.text_input("Número ID da escola:", value="", placeholder="Digite aqu
 if foto is not None:
     st.image(foto, caption="Foto do aluno", width=100)
 
+ids_escolas = [
+    "123456",
+    "12345",
+    "1234",
+    "123",
+    "12",
+    "1"
+]
+
 campos_aluno = {
     'Nome': nome,
     'CPF': cpf,
     'Responsável': responsavel,
-    'Foto': foto
+    'Foto': foto,
+    'Id da ecola': id_es
 }
 
+
+
 if st.button("Concluir"):
-    if ver_preenchidos(campos_aluno):
+
+    if ver_preenchidos(campos_aluno) and id_es in ids_escolas:
         st.success("Concluído com sucesso!")
         sleep(0.5)
         st.switch_page("pages/home_aluno.py")
     
+    elif id_es not in ids_escolas:
+        st.error("Este ID não está cadastrado.")
+
+
     else:
         st.error("Por favor, preencha todos os campos.")
     
